69fad06d5fc7bf24a681671749ff0dc20f2bbbdf
221900323 <221900323@smail.nju.edu.cn>
2025-05-26 10:53:15 +0800

    modify config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-vue-markdown'
 
 export default defineConfig({
-  base: '/blog/',
+  base: '/blog',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
