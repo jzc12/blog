dd4ce5d8f7d84fbc995f0be86ff4a53bf25456ea
221900323 <221900323@smail.nju.edu.cn>
2025-05-30 21:42:04 +0800

    modify size

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './css/style.css'
 import './css/lapis.css'
+import './assets/styles/katex-fonts.css'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
