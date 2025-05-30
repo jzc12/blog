3677294bd09b328030e3c16eb8acb57e80f44223
jzc12 <jzc12@users.noreply.github.com>
2025-05-30 13:34:54 +0000

    deploy: 4beb663c06bb06d18addc0d59d5e4506e680a664

---
+++ assets/about-24ded698.js
@@ -0,0 +1,2 @@
+import{o as n,c as l,b as o}from"./index-27777751.js";const r={class:"markdown-body"},u={__name:"about",setup(a,{expose:t}){return t({frontmatter:{}}),(c,e)=>(n(),l("div",r,e[0]||(e[0]=[o("h1",null,"关于",-1),o("h2",null,"coding",-1),o("p",null,"@九曲乡河学院—软件开发",-1),o("p",null,"测试",-1),o("pre",null,[o("code",{class:"language-python"},`print("Hello, world!")
+`)],-1)])))}};export{u as default};
