02c746e4eb73d39b02a362b99d1268af3c2a6043
jzc12 <jzc12@users.noreply.github.com>
2025-05-31 07:41:37 +0000

    deploy: ce289d066d1a6b251f9a3786bc6eda9224b61da5

--- assets/README-d0602775.js
+++ assets/README-d0602775.js
@@ -1,4 +1,4 @@
-import{o as e,c as d,a}from"./index-7a689b3c.js";const r={class:"markdown-body"},c={__name:"README",setup(o,{expose:n}){return n({frontmatter:{}}),(s,t)=>(e(),d("div",r,t[0]||(t[0]=[a(`<h1>ner_kge</h1><h2>结构框架</h2><pre><code class="language-bash">
+import{o as e,c as d,a}from"./index-08a29e25.js";const r={class:"markdown-body"},c={__name:"README",setup(o,{expose:n}){return n({frontmatter:{}}),(s,t)=>(e(),d("div",r,t[0]||(t[0]=[a(`<h1>ner_kge</h1><h2>结构框架</h2><pre><code class="language-bash">
 ner_kge/
 ├── data/
 │  		├── conll2003/       # 原始数据集
