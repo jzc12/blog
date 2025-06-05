39cf0959e937a8dba8c1a23883ca728585650f70
jzc12 <jzc12@users.noreply.github.com>
2025-06-05 16:40:15 +0000

    deploy: 8271e65e6e9c312f27e953bd952fe0d24f67d77a

--- assets/about-3c4d929b.js
+++ assets/about-3c4d929b.js
@@ -1,4 +1,4 @@
-import{o as t,c as r,a as l}from"./index-40f665b5.js";const a={class:"markdown-body"},d={__name:"about",setup(n,{expose:e}){return e({frontmatter:{}}),(s,o)=>(t(),r("div",a,o[0]||(o[0]=[l(`<h1>关于</h1><h2>coding</h2><p>@九曲乡河学院—软件开发</p><p>测试</p><pre><code class="language-python">print(&quot;Hello, world!&quot;)
+import{o as t,c as r,a as l}from"./index-4410fcb4.js";const a={class:"markdown-body"},d={__name:"about",setup(n,{expose:e}){return e({frontmatter:{}}),(s,o)=>(t(),r("div",a,o[0]||(o[0]=[l(`<h1>关于</h1><h2>coding</h2><p>@九曲乡河学院—软件开发</p><p>测试</p><pre><code class="language-python">print(&quot;Hello, world!&quot;)
 </code></pre><hr><pre><code class="language-c++">cout&lt;&lt;&quot;Hello, world!&quot;&lt;&lt;endl;
 </code></pre><hr><pre><code class="language-java">System.println(&quot;Hello, world!&quot;)
 </code></pre><hr><pre><code class="language-c">printf(&quot;Hello, world!\\n&quot;);
