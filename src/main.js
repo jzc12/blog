1a889b370f1deb972a9064216acbdda04ffe6f05
221900323 <221900323@smail.nju.edu.cn>
2025-06-09 17:17:02 +0800

    modify back

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import './css/style.css'
 import './css/lapis.css'
 import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import background1 from './assets/background1.png';
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -15,5 +16,14 @@ pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+    const img = new Image();
+    img.src = background1;
+    img.onload = () => {
+        body.style.backgroundImage = `url(${img.src})`;
+    };
+});
 
 
