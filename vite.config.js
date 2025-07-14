94d6e913743167c3e23f54b42db9a5edcf943287
jzc <3067665180@qq.com>
2025-07-14 23:38:33 +0800

    冲突测试

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ const markdown = MarkdownIt({
   .use(MarkdownItPrism)
 
 export default defineConfig({
-  base: '/',
+  base: mode === 'github' ? '/blog/' : '/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
